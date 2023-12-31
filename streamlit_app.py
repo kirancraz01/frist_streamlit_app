56a6e88c53c2fce4fd4d1eb543628ff4b4f93a3c
kirancraz01 <104181185+kirancraz01@users.noreply.github.com>
2023-12-31 16:07:57 +0530

    Update streamlit_app.py
    json added

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,4 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
