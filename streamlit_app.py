1f5afb44dabed65b35e4cfff742d12452d781bf8
kirancraz01 <104181185+kirancraz01@users.noreply.github.com>
2023-12-31 16:11:17 +0530

    Update streamlit_app.py
    json to df

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,19 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
-# # Let's put a pick list here so they can pick the fruit they want to include 
-# my_fruit_list = my_fruit_list.set_index('Fruit')
-# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
+# Let's put a pick list here so they can pick the fruit they want to include 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-# fruits_to_show = my_fruit_list.loc[fruits_selected]
-# streamlit.dataframe(fruits_to_show)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
